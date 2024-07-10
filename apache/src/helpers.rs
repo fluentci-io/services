e3b71b6bfcf10d4ea0a19514f720d5b373811522
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-10 18:06:38 +0000

    add start/stop functions

--- apache/src/helpers.rs
+++ apache/src/helpers.rs
@@ -1,6 +1,5 @@
 use anyhow::Error;
 use fluentci_pdk::dag;
-use fluentci_types::nix::NixArgs;
 
 pub fn setup() -> Result<String, Error> {
     dag()
@@ -8,10 +7,45 @@ pub fn setup() -> Result<String, Error> {
         .with_exec(vec!["mkdir", "-p", ".fluentci"])?
         .stdout()?;
 
+    let pwd = dag().get_env("PWD")?;
+    let httpd_port = dag().get_env("HTTPD_PORT")?;
+    let httpd_confdir = dag().get_env("HTTPD_CONFDIR")?;
+    let httpd_error_log_file = dag().get_env("HTTPD_ERROR_LOG_FILE")?;
+    let httpd_access_log_file = dag().get_env("HTTPD_ACCESS_LOG_FILE")?;
+
+    if httpd_port.is_empty() {
+        dag().set_envs(vec![("HTTPD_PORT".into(), "8080".into())])?;
+    }
+
+    if httpd_confdir.is_empty() {
+        dag().set_envs(vec![("HTTPD_CONFDIR".into(), format!("{}/.fluentci", pwd))])?;
+    }
+
+    if httpd_error_log_file.is_empty() {
+        dag().set_envs(vec![(
+            "HTTPD_ERROR_LOG_FILE".into(),
+            format!("{}/.fluentci/log/error.log", pwd),
+        )])?;
+    }
+
+    if httpd_access_log_file.is_empty() {
+        dag().set_envs(vec![(
+            "HTTPD_ACCESS_LOG_FILE".into(),
+            format!("{}/.fluentci/log/access.log", pwd),
+        )])?;
+    }
+
     let stdout = dag()
         .flox()?
         .with_workdir(".fluentci")?
-        .with_exec(vec!["flox", "install", "apacheHttpd", "overmind"])?
+        .with_exec(vec!["flox", "install", "apacheHttpd", "overmind", "tmux", "wget", "curl"])?
+        .with_exec(vec!["mkdir", "-p", "log"])?
+        .with_exec(vec![r#"[ -f log/.gitignore ] || echo -e 'error.log\naccess.log' > log/.gitignore"#])?
+        .with_exec(vec!["[ -f httpd.conf ] || wget https://raw.githubusercontent.com/fluentci-io/services/main/apache/httpd.conf"])?
+        .with_exec(vec!["[ -f index.html ] || wget https://raw.githubusercontent.com/fluentci-io/services/main/apache/web/index.html"])?
+        .with_exec(vec![
+            "grep -q web Procfile || echo 'web: apachectl start -f $PWD/httpd.conf -D FOREGROUND' >> Procfile",
+        ])?
         .stdout()?;
 
     Ok(stdout)
--- apache/src/lib.rs
+++ apache/src/lib.rs
@@ -13,6 +13,24 @@ pub fn start(_args: String) -> FnResult<String> {
         .with_exec(vec!["httpd", "-v"])?
         .with_exec(vec!["type", "overmind"])?
         .with_exec(vec!["type", "httpd"])?
+        .with_exec(vec![
+            "echo -e \"Apache starting on port $HTTPD_PORT\\n http://localhost:$HTTPD_PORT\"",
+        ])?
+        .with_exec(vec!["overmind", "start", "-f", "Procfile", "--daemonize"])?
+        .with_exec(vec!["sleep", "3"])?
+        .with_exec(vec!["overmind", "status"])?
+        .with_exec(vec!["curl", "-s", "http://localhost:$HTTPD_PORT"])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
+pub fn stop(args: String) -> FnResult<String> {
+    helpers::setup()?;
+    let stdout = dag()
+        .flox()?
+        .with_workdir(".fluentci")?
+        .with_exec(vec!["overmind", "stop", &args])?
         .stdout()?;
     Ok(stdout)
 }
