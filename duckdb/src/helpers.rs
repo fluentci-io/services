d821ae746f1f6e0782568d2bca61a90a4a6dbc27
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-07-11 07:01:02 +0000

    run tests on macos

--- duckdb/src/helpers.rs
+++ duckdb/src/helpers.rs
@@ -1,7 +1,21 @@
 use anyhow::Error;
 use fluentci_pdk::dag;
 
+pub fn setup_flox() -> Result<(), Error> {
+    let os = dag().get_os()?;
+    if os == "macos" {
+        dag()
+        .pipeline("setup-flox")?
+        .with_exec(vec![r#"type brew > /dev/null 2> /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""#])?
+        .with_exec(vec!["type flox > /dev/null 2> /dev/null || brew install flox"])?
+        .stdout()?;
+    }
+    Ok(())
+}
+
 pub fn setup() -> Result<String, Error> {
+    setup_flox()?;
+
     dag()
         .pipeline("setup")?
         .with_exec(vec!["mkdir", "-p", ".fluentci"])?
