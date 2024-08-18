5bde4c5270e53b128c24e97b4bc8759b3a6add75
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-18 13:15:30 +0000

    feat(mysql): create database if MYSQL_* env are defined

--- mysql/src/lib.rs
+++ mysql/src/lib.rs
@@ -6,7 +6,23 @@ pub mod helpers;
 #[plugin_fn]
 pub fn start(_args: String) -> FnResult<String> {
     helpers::setup()?;
+    let user = dag().get_env("MYSQL_USER")?;
+    let password = dag().get_env("MYSQL_PASSWORD")?;
+    let database = dag().get_env("MYSQL_DATABASE")?;
     let port = dag().get_env("MYSQL_PORT")?;
+
+    if user.is_empty() {
+        dag().set_envs(vec![("MYSQL_USER".into(), "fluentci".into())])?;
+    }
+
+    if password.is_empty() {
+        dag().set_envs(vec![("MYSQL_PASSWORD".into(), "fluentci".into())])?;
+    }
+
+    if database.is_empty() {
+        dag().set_envs(vec![("MYSQL_DATABASE".into(), "demo".into())])?;
+    }
+
     let stdout = dag()
         .flox()?
         .with_workdir(".fluentci/mysql")?
@@ -20,6 +36,30 @@ pub fn start(_args: String) -> FnResult<String> {
         ])?
         .wait_on(port.parse()?, None)?
         .with_exec(vec!["cat", "$MYSQL_HOME/mysql.log"])?
+        .with_exec(vec![
+            "mysql",
+            "-u",
+            "root",
+            "--socket=$MYSQL_HOME/mysql.socket -e \"CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\"",
+        ])?
+        .with_exec(vec![
+            "mysql",
+            "-u",
+            "root",
+            "--socket=$MYSQL_HOME/mysql.socket -e \"CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';\"",
+        ])?
+        .with_exec(vec![
+            "mysql",
+            "-u",
+            "root",
+            "--socket=$MYSQL_HOME/mysql.socket -e \"GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';\"",
+        ])?
+        .with_exec(vec![
+            "mysql",
+            "-u",
+            "root",
+            "--socket=$MYSQL_HOME/mysql.socket -e \"FLUSH PRIVILEGES;\"",
+        ])?
         .with_exec(vec!["overmind", "status"])?
         .stdout()?;
     Ok(stdout)
